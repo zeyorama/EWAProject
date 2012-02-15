140f6c9c53c16792a600a083f0b029da71e6396b
Frank Kevin Zey <fkz.dev@hotmail.de>
2012-02-15 10:51:18 +0100

    one more merge commit

--- lib/cGenre.php
+++ lib/cGenre.php
@@ -1,11 +1,14 @@
 <?php	
-
 	include_once 'lib/iGenre.php';	
 
 	final class Genre implements i_genre {
 		private $genre_id;
 		private $name;
 		
+		/**
+		 * Constructor for this class.
+		 * @param optional: genre_id to set id, name to set name
+		 */
 		public function __construct() {
 			if(func_num_args() == 2) {
 				$this->genre_id = func_get_arg(0);
@@ -15,28 +18,28 @@
 		
 		/* (non-Javadoc)
 		 * @see i_genre#getIndex
-		*/
+		 */
 		public function getIndex() {
 			return $this->genre_id;
 		}
 		
 		/* (non-Javadoc)
 		 * @see i_genre#instance
-		*/
+		 */
 		public function getGenre() {
 			return $this->name;
 		}
 		
 		/* (non-Javadoc)
 		 * @see i_genre#instance
-		*/
+		 */
 		public function setGenre($genre) {
 			$this->name = $genre;
 		}
 		
 		/* (non-Javadoc)
 		 * @see i_genre#instance
-		*/
+		 */
 		public function setIndex($index) {
 			$this->genre_id = $index;
 		}
