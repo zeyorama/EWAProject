66afed2d50960d3f6b4862a092200b622a856de7
Quallenmann <quallenmann@Kiori.localdomain>
2012-02-15 17:55:11 +0100

    Database workaround for get_result()

--- epic.php
+++ epic.php
@@ -11,9 +11,11 @@ if (file_exists('config.php')) {
   exit;
 }
 
-login("Quallenmann", "EWAProjectPass");
+//login("Quallenmann", "EWAProjectPass");
 
-var_dump(signed_in());
+//$db->query("SELECT * FROM _user");
+//echo "<br>";
+//var_dump(serialize($db->get_next_result("User")));
 
 $gen->set_title("Test");
 $gen->add_content("<div>Miau</div>");
