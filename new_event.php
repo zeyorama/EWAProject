f8ee038eb0ce46d3f8d81c75779f4c4418d47481
Frank Kevin Zey <frank.kevin.zey@mni.th-mittelhessen.de>
2012-02-16 15:52:08 +0100

    event creation bug fix #1

--- new_event.php
+++ new_event.php
@@ -2,9 +2,31 @@
 
   include 'epic.php';
   
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $location = $_POST['location'];
+  $start = $_POST['start'];
+  
+  if ($loaction == "") {
+    $location = "undef";
+  }
+  
+  if ($name == "") {
+    $name = "noname";
+  }
+  
+  if ($description == "") {
+    $description = "no description";
+  }
+  
+  if ($start == "") {
+    $start = date('Y-m-d H:i:s');
+  }
+  
+  
   global $db;
   $db->prepare("INSERT INTO _event(startDate,location,description,owner_user_id,locked,FSK,name) VALUES(?,?,?,{$_POST['id']},1,0,?);");
-  $db->exe_prepare('ssss',$_POST['start'], $_POST['location'], $_POST['description'], $_POST['name']);
+  $db->exe_prepare('ssss',$start, $location, $description, $name);
   
   header("Location: index.php?events");
 
