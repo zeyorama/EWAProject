5fc4c65583aad81acd0a045c2e7e2eb30ce69016
Quallenmann <quallenmann@Kiori.localdomain>
2012-02-22 22:01:55 +0100

    little die fix

--- register.php
+++ register.php
@@ -10,14 +10,17 @@ $pass = $_POST['pass'];
 if (strlen($nick) < 5) {
   $_SESSION['err'] = 'nickname at least 5 characters';
   header('Location: index.php?register');
+  die;
 }
 if (!preg_match("([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))", $email)) {
   $_SESSION['err'] = 'email formate incorrect';
   header('Location: index.php?register');
+  die;
 }
 if (strlen($pass) < 5) {
   $_SESSION['err'] = 'password at least 5 characters';
   header('Location: index.php?register');
+  die;
 }
 
 $db->query("SELECT * FROM _user WHERE nick='$nick';");
@@ -25,6 +28,7 @@ $db->query("SELECT * FROM _user WHERE nick='$nick';");
 if ($db->get_length() > 0) {
   $_SESSION['err'] = "$nick allready in use, try another one";
   header('Location: index.php?register');
+  die;
 }
 
 $db->query("SELECT * FROM _user WHERE email='$email';");
@@ -32,6 +36,7 @@ $db->query("SELECT * FROM _user WHERE email='$email';");
 if ($db->get_length() > 0) {
   $_SESSION['err'] = "$email allready in use, choose another one";
   header('Location: index.php?register');
+  die;
 }
 
 $p = md5($pass);
