a387340f6937e7fd97d6dc7a409f4b5eed24974a
Frank Kevin Zey <frank.kevin.zey@mni.th-mittelhessen.de>
2012-03-15 16:21:03 +0100

    Fixes little mistake

--- config.pl
+++ config.pl
@@ -70,4 +70,4 @@ open(_F, ">$cf");
   print(_F "?>\n");
 close(_F);
 system('clear');
-print("Database, user and tables created. Have fun with EWSProject!\n\n");
+print("Database, user and tables created. Have fun with EWAProject!\n\n");
